f7ab2641d0c71322ae8f89a61ba93cbfae4c41be
serkanoezkan <serkoezkan@gmail.com>
2024-12-12 16:07:41 +0300

    styling + improvements

--- scripts.js
+++ scripts.js
@@ -4,9 +4,9 @@
         let next = doc.querySelector("next");
         let step = 1;
 
-        let questions = ["Wer hat Algebra erfunden?", "Wie alt ist die Türkische Sprache?", "Welcher Fluss hat am meisten Wasser auf der Welt?"];
-        let answer = [["Diophantos von Alexandria", "Al-Chwarizmi", "Archimedes", "Fermat"], ["100 Jahre", "3000 Jahre", "6000 Jahre", "600 Jahre"], ["Nil", "Rhein", "Kongo", "Amazonas"]];
-        let rightChoices = ["Al-Chwarizmi", "6000 Jahre", "Amazonas"];
+        let questions = ["Wer hat Algebra erfunden?", "Wie alt ist die Türkische Sprache?", "Welcher Fluss hat am meisten Wasser auf der Welt?", "Wo im menschlichen Körper befindet sich der kleinste Knochen?"];
+        let answer = [["Diophantos von Alexandria", "Al-Chwarizmi", "Archimedes", "Fermat"], ["100 Jahre", "3000 Jahre", "6000 Jahre", "600 Jahre"], ["Nil", "Rhein", "Kongo", "Amazonas"], ["Im Ohr", "Im Finger", "Im Zäh"]];
+        let rightChoices = ["Al-Chwarizmi", "6000 Jahre", "Amazonas", "Im Ohr"];
 
         if(step == 1){
             question.innerText = questions[0];
@@ -21,14 +21,17 @@
             x.setAttribute("id", "Antwort" + i);
             x.addEventListener("click", () => {
             if(x.textContent === rightChoices[0] && step == 1){
-                x.style.color = "green";
-                x.style.border = "1px solid green";
+                x.style.color = "#16A34A";
+                x.style.border = "1px solid #6EE7B7";
                 step++;
                 console.log(step);
             }else if(x.textContent === rightChoices[1] && step == 2){
                 console.log("Richtig");
                 step++;
             }else if(x.textContent === rightChoices[2] && step == 3){
+                console.log("Richtig");
+                step++;
+            }else if(x.textContent === rightChoices[3] && step == 4){
                 console.log("Richtig");
                 step++;
             }else{
@@ -50,11 +53,23 @@
                 }
             }else if(step == 3){
                 question.innerText = questions[2];
-                for(let i = 0; i < answer[1].length; i++){
+                for(let i = 0; i < answer[2].length; i++){
                     let q = doc.getElementById("Antwort" + [i]);
                     q.innerText = answer[2][i];
                     q.style.color = "black";
                     q.style.border = "none";
                 }
+            }else if(step == 4){
+            question.innerText = questions[3];
+            if(answer[3].length < answer[2].length){
+                let element = doc.getElementById("Antwort3");
+                element.remove();
             }
+            for(let i = 0; i < answer[3].length; i++){
+                let q = doc.getElementById("Antwort" + [i]);
+                q.innerText = answer[3][i];
+                q.style.color = "black";
+                q.style.border = "none";
+            }
+        }
         });
