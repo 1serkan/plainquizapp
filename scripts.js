09a8ba0cd2b054ce8af7dac3611a832b6c377668
serkanoezkan <serkoezkan@gmail.com>
2024-12-14 12:55:21 +0300

    bugfixes + improvements

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
-        let doc = document;
-        let question = doc.querySelector("question");
-        let answers = doc.querySelector("answer");
-        let next = doc.querySelector("next");
+        const doc = document;
+        const question = doc.querySelector("question");
+        const answers = doc.querySelector("answer");
+        const next = doc.querySelector("next");
+        const message = doc.querySelector("message");
+        const box = doc.getElementById("box");
         let step = 1;
+        let isClicked = false;
 
         let questions = ["Wer hat Algebra erfunden?", "Wie alt ist die Türkische Sprache?", "Welcher Fluss hat am meisten Wasser auf der Welt?", "Wo im menschlichen Körper befindet sich der kleinste Knochen?"];
         let answer = [["Diophantos von Alexandria", "Al-Chwarizmi", "Archimedes", "Fermat"], ["100 Jahre", "3000 Jahre", "6000 Jahre", "600 Jahre"], ["Nil", "Rhein", "Kongo", "Amazonas"], ["Im Ohr", "Im Finger", "Im Zäh"]];
@@ -15,14 +18,54 @@
             }
         }
 
+        function toggleColors(x, b, m, n, state){
+            let sborder;
+            let scolor;
+            let bcolor;
+            let mcolor;
+            let mbgcolor;
+            let mText;
+            let nText;
+            
+            if(state == "success"){
+                sborder = "1px solid #6EE7B7";
+                scolor = "#16A34A";
+                bcolor = "0.2rem solid #6EE7B7";
+                mcolor = "#6EE7B7";
+                mbgcolor = "rgb(60, 131, 81)";
+                mText = "Du hast die richtige Auswahl getroffen\n Klicke auf Weiter, um fortzufahren.";
+                nText = "Weiter";
+            }else{
+                sborder = "1px solid rgb(231, 110, 110)";
+                scolor = "#a31616";
+                bcolor = "0.2rem solid rgb(231, 110, 110)";
+                mcolor = "#e76e6e";
+                mbgcolor = "rgb(131, 60, 60)";
+                mText = "Du hast die falsche Auswahl getroffen\n Klicke auf Wiederholen, um es erneut zu versuchen.";
+                nText = "Wiederholen";
+            }
+            x.style.border = sborder;
+            x.style.color = scolor;
+            b.style.border = bcolor;
+            m.style.color = mcolor;
+            m.style.backgroundColor = mbgcolor;
+            m.style.border = sborder;
+            m.innerText = mText;
+            n.style.backgroundColor = mbgcolor;
+            n.style.border = sborder;
+            n.innerText = nText;
+        }
+
         function checkSelected(x, i){
+            if(isClicked === false){
             if(x.textContent === rightChoices[i]){
-                x.style.color = "#16A34A";
-                x.style.border = "1px solid #6EE7B7";
+                isClicked = true;
+                toggleColors(x, box, message, next, "success");
                 step++;
             }else {
-                x.style.color = "red";
-                x.style.border = "1px solid rgb(231, 110, 110)";
+                isClicked = true;
+                toggleColors(x, box, message, next, "failure");
+                }
             }
         }
 
@@ -30,47 +73,57 @@
             x = doc.createElement("p");
             x.innerText = answer[0][i];
             x.setAttribute("id", "Antwort" + i);
-            x.addEventListener("click", () => {
+            if(isClicked === false){
+                x.addEventListener("mouseover", () => {!isClicked ? x.style.borderBottom = "1px solid #1E3A8A" : null});
+                x.addEventListener("mouseleave", () => {!isClicked ? x.style.borderBottom = "1px solid #e5e5e5" : null})
+                x.addEventListener("click", () => {
             switch(step){
                 case 1: checkSelected(x, 0); break;
                 case 2: checkSelected(x, 1); break;
                 case 3: checkSelected(x, 2); break;
                 case 4: checkSelected(x, 3); break;
-            }
-        })
+                }       
+            })
+        }
             answers.appendChild(x);
             return x;
         }
 
         function refreshContent(a){
-                question.innerText = questions[a];
-                for(let i = 0; i < answer[a].length; i++){
-                    let q = doc.getElementById("Antwort" + [i]);
-                    q.innerText = answer[a][i];
-                    q.style.color = "black";
-                    q.style.border = "none";
-                }
+            question.innerText = questions[a];
+            for(let i = 0; i < answer[a].length; i++){
+                let q = doc.getElementById("Antwort" + [i]);
+                q.innerText = answer[a][i];
+                q.style.color = "black";
+                q.style.border = "none";
+            }
         }
 
         function end(){
             question.innerText = "Glückwunsch du hast den Test abgeschlossen!";
             question.style.fontSize = "1rem";
+            message.remove();
             next.remove();
             answers.remove();
         }
 
-        function checkAnswerSize(asmallerThan, stIndex, ahigherThan, htIndex){
-            if(asmallerThan[stIndex].length < ahigherThan[htIndex].length){
-                let e = doc.getElementById("Antwort" + (ahigherThan[htIndex].length - 1));
-                e.remove();
+        function checkAnswerSize(array, stIndex, htIndex){
+            try{
+                if(array[stIndex].length < array[htIndex].length){
+                    let e = doc.getElementById("Antwort" + (array[htIndex].length - 1));
+                    e.remove();
+                }
+            }catch(err){
+                console.log(err);
             }
         }
 
         next.addEventListener("click", () => {
             switch(step){
-                case 2: refreshContent(1); break;
-                case 3: refreshContent(2); break;
-                case 4: refreshContent(3); checkAnswerSize(answer, 3, answer, 2); break;
+                case 1: refreshContent(0); isClicked = false; break;
+                case 2: refreshContent(1); isClicked = false; break;
+                case 3: refreshContent(2); isClicked = false; break;
+                case 4: refreshContent(3); checkAnswerSize(answer, 3, 2); isClicked = false; break;
                 default: end();
             }
         });
